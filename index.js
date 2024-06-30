b85178b5e3b3bbb8f5654caf84cbaabc274990c3
Moddy-t <amalembamodesta@gmail.com>
2024-06-30 23:53:15 +0300

    complete function lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution in this file!
+// Code your solution in this file
+
+function distanceFromHqInBlocks(street) {
+    const hqStreet = 42;
+    return Math.abs(street - hqStreet);
+  }
+  function distanceFromHqInFeet(street) {
+    const hqStreet = 42;
+    const feetPerBlock = 264;
+    const distanceInBlocks = Math.abs(street - hqStreet);
+    return distanceInBlocks * feetPerBlock;
+  }
+  function distanceTravelledInFeet(start, end) {
+    const feetPerBlock = 264;
+    return Math.abs(start - end) * feetPerBlock;
+  }
+  function distanceFromHqInFeet(street) {
+    const hqStreet = 42;
+    const feetPerBlock = 264;
+    const distanceInBlocks = Math.abs(street - hqStreet);
+    return distanceInBlocks * feetPerBlock;
+  }
+  function distanceTravelledInFeet(start, destination) {
+    const feetPerBlock = 264;
+    const distanceInBlocks = Math.abs(start - destination);
+    return distanceInBlocks * feetPerBlock;
+  }
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    
+    if (distance <= 400) {
+      return 0; 
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02; 
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
+
+
+
